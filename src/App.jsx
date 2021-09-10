efc3c2517b8466a1e98b8b5dcee03adda1920439
William <will2b@gmail.com>
2021-09-10 12:39:16 -0700

    add Inter font

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,24 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <header>
+        <nav>
+          <ul id="nav-list">
+            <li className="nav-item">
+              <a href="#">菜單</a>
+            </li>
+            <li className="nav-item">
+              <a href="#">我要訂單</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+
+      <main></main>
+    </div>
+  );
 }
 
 export default App;
