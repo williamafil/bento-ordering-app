6c5d34b8a48496b551c045937d4e8379cdd7a196
William <will2b@gmail.com>
2021-09-12 13:30:51 -0700

    move dummy data to single file

---
+++ src/data/dummy-data.js
@@ -0,0 +1,232 @@
+const DUMMY_DATA = {
+  mainCourse: {
+    header: "主食『擇一』",
+    options: [
+      {
+        name: "蔬食沙拉",
+        price: 95,
+      },
+      {
+        name: "舒肥嫩雞胸",
+        price: 129,
+      },
+      {
+        name: "義式香料豬",
+        price: 139,
+      },
+      {
+        name: "慢火燉牛肉",
+        price: 149,
+      },
+      {
+        name: "炙烤煙燻鮭魚",
+        price: 149,
+      },
+      {
+        name: "鹽水虱目魚肚",
+        price: 149,
+      },
+    ],
+  },
+  sauce: {
+    header: "醬汁『擇一』",
+    options: [
+      {
+        name: "檸檬油醋",
+      },
+      {
+        name: "義式油醋",
+      },
+      {
+        name: "和風油醋",
+      },
+      {
+        name: "胡麻醬",
+      },
+      {
+        name: "塔塔醬(適合雞肉、魚類主餐)",
+      },
+      {
+        name: "不要醬汁",
+      },
+    ],
+  },
+  starch: {
+    header: "澱粉類『擇一』",
+    options: [
+      {
+        name: "紅藜麥飯",
+      },
+      {
+        name: "冰心地瓜",
+      },
+      {
+        name: "各半",
+      },
+      {
+        name: "不要",
+      },
+      {
+        name: "餐盒都有少許生菜，不加生菜請提前告知",
+      },
+    ],
+  },
+  veggie: {
+    header: "蔬果類『擇七』 - 超過 7 樣，每樣酌收 $15",
+    options: [
+      {
+        name: "隨機（請告知飲食禁忌）",
+        price: 15,
+      },
+      {
+        name: "花椰菜",
+        price: 15,
+      },
+      {
+        name: "西洋芹",
+        price: 15,
+      },
+      {
+        name: "小黃瓜",
+        price: 15,
+      },
+      {
+        name: "紅蘿蔔",
+        price: 15,
+      },
+      {
+        name: "杏鮑菇",
+        price: 15,
+      },
+      {
+        name: "苜蓿芽",
+        price: 15,
+      },
+      {
+        name: "鷹嘴豆",
+        price: 15,
+      },
+      {
+        name: "紅扁豆",
+        price: 15,
+      },
+      {
+        name: "毛豆",
+        price: 15,
+      },
+      {
+        name: "南瓜",
+        price: 15,
+      },
+      {
+        name: "玉米粒",
+        price: 15,
+      },
+      {
+        name: "豆薯",
+        price: 15,
+      },
+      {
+        name: "海帶",
+        price: 15,
+      },
+      {
+        name: "豆乾",
+        price: 15,
+      },
+      {
+        name: "秋葵",
+        price: 15,
+      },
+      {
+        name: "木耳",
+        price: 15,
+      },
+      {
+        name: "生洋蔥",
+        price: 15,
+      },
+      {
+        name: "彩椒",
+        price: 15,
+      },
+      {
+        name: "竹筍",
+        price: 15,
+      },
+      {
+        name: "玉米筍",
+        price: 15,
+      },
+      {
+        name: "油封蒜頭",
+        price: 15,
+      },
+      {
+        name: "醋漬洋蔥",
+        price: 15,
+      },
+      {
+        name: "水煮蛋",
+        price: 15,
+      },
+      {
+        name: "溏心蛋",
+        price: 15,
+      },
+      {
+        name: "水波蛋",
+        price: 15,
+      },
+      {
+        name: "小番茄",
+        price: 15,
+      },
+      {
+        name: "酪梨",
+        price: 15,
+      },
+      {
+        name: "葡萄",
+        price: 15,
+      },
+      {
+        name: "芭樂",
+        price: 15,
+      },
+      {
+        name: "鳳梨",
+        price: 15,
+      },
+      {
+        name: "香蕉",
+        price: 15,
+      },
+    ],
+  },
+  nuts: {
+    header: "堅果類『擇一』",
+    options: [
+      {
+        name: "南瓜子",
+      },
+      {
+        name: "杏仁果",
+      },
+      {
+        name: "核桃",
+      },
+      {
+        name: "腰果",
+      },
+      {
+        name: "綜合",
+      },
+      {
+        name: "不要堅果",
+      },
+    ],
+  },
+};
+
+export default DUMMY_DATA;
