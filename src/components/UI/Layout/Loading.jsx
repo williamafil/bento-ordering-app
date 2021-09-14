c217a1d36779bf232c551cecd6de9975f10bdd51
William <will2b@gmail.com>
2021-09-13 18:03:19 -0700

    add isLoading

---
+++ src/components/UI/Layout/Loading.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Loading = () => {
+  return (
+    <div className="fixed h-screen w-full bg-white flex justify-center items-center">
+      <p className="text-black font-light text-2xl">Loading...</p>
+    </div>
+  );
+};
+
+export default Loading;
